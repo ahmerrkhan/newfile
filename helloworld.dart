f99c126557952dd18493103e647ca78446020a47
Muhammad Ahmer Khan <70189037+ahmerrkhan@users.noreply.github.com>
2021-03-10 18:33:09 +0000

    code examples dart

---
+++ helloworld.dart
@@ -0,0 +1,19 @@
+import 'dart:ffi';
+
+void main() {
+  //this is single line comment
+  print('Hello, World!');
+  /*this 
+  is multiple line comment*/
+  print("Hello, Good Afternoon!");
+
+  ///This is documentaion in
+  ///dart programming language
+  print("Hello, Good Morning!");
+
+  //Data types
+  int marks1 = 290;
+  print("Marks 1 : ${marks1}");
+  double marks2 = 23.44;
+  print("Marks 2 : ${marks2}");
+}
